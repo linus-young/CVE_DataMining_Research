da8d50fa4c823c725eaba16e5a67dba14b9b225b
Power Snail <powersnail@gmail.com>
2016-06-28 21:33:11 -0400

    m

--- scripts/cwe_tree_mine.rb
+++ scripts/cwe_tree_mine.rb
@@ -1,13 +1,14 @@
 require 'net/http'
 
-url = URI.parse('http://www.google.com')
+url = URI.parse('http://cwe.mitre.org/data/definitions/2.html')
 req = Net::HTTP::Get.new(url.to_s)
 res = Net::HTTP.start(url.host, url.port) {|http|
   http.request(req)
 }
 str = res.body
-id = str.index('ChildOf')
-while id != nil do
+puts str
+# id = str.index('ChildOf')
+# while id != nil do
     
-    id = str.index('ChildOf')
-end
+#     id = str.index('ChildOf')
+# end
